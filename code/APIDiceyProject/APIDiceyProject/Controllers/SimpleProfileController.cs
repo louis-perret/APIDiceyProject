0b65c0e9d1d32eb78c57dd8c804e9a405d889a08
Neitah MALVEZIN <bamalvezin>
2023-01-28 09:37:21 +0100

    legere modif sur SimpleProfileController

--- code/APIDiceyProject/APIDiceyProject/Controllers/SimpleProfileController.cs
+++ code/APIDiceyProject/APIDiceyProject/Controllers/SimpleProfileController.cs
@@ -1,6 +1,11 @@
-﻿namespace APIDiceyProject.Controllers
+﻿using Api.Services;
+
+namespace APIDiceyProject.Controllers
 {
-    public class SimpleProfileController
+    public class SimpleProfileController : AbstractProfileController
     {
+        protected SimpleProfileController(ILogger<AbstractDiceController> logger, IProfileService profileService) : base(logger, profileService)
+        {
+        }
     }
 }
