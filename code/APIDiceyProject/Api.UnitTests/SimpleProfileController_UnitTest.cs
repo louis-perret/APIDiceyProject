d1ea80aafcb272f02d8da602ed759101ba05e9e5
Neitah MALVEZIN <neitah.malvezin@etu.uca.fr>
2023-02-11 17:05:30 +0100

    Ajout des tests pour Get et Remove

--- code/APIDiceyProject/Api.UnitTests/SimpleProfileController_UnitTest.cs
+++ code/APIDiceyProject/Api.UnitTests/SimpleProfileController_UnitTest.cs
@@ -2,7 +2,10 @@
 using Api.Services.ProfileFolder;
 using APIDiceyProject.Controllers;
 using APIDiceyProject.Controllers.ProfileFolder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.Logging.Abstractions;
+using ModelDTOExtensions;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -31,21 +34,44 @@ namespace Api.UnitTests
         {
             var loggerApi = new NullLogger<AbstractProfileController>();
             var service = new Mock<IProfileService>();
+            
             service.Setup(service => service.GetProfilesByPage(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>()))
-                .Returns(Task.FromResult(CreateDatasetProfile()));
+                .Returns(new Func<int,int,string, Task<List<Profile>>>((int numPage, int NbByPage, string subStr) => Task.FromResult(CreateDatasetProfile().Where(profile => (profile.Name+profile.Surname).Contains(subStr) || (profile.Surname + profile.Name).Contains(subStr)).ToList())));
+
             service.Setup(service => service.GetProfileById(It.IsAny<Guid>()))
             .Returns(new Func<Guid, Task<Model.Profile?>>((id) => Task.FromResult(CreateDatasetProfile().Where(profile => profile.Id == id).FirstOrDefault())));
+            
             service.Setup(service => service.RemoveAllProfiles())
                 .Returns(new Func<Task<bool>>(() => Task.FromResult(true)));
+            
             service.Setup(service => service.RemoveProfileById(It.IsAny<Guid>()))
-                .Returns(new Func<Guid, Task<bool>>(id => Task.FromResult(SimulatedRemoveDiceById(id))));
+                .Returns(new Func<Guid, Task<bool>>(id => Task.FromResult(SimulatedRemoveProfileById(id))));
+            
             service.Setup(service => service.AddProfile(It.IsAny<Model.Profile>()))
-                .Returns(new Func<Model.Profile, Task<bool>>(profile => Task.FromResult(SimulatedAddProfile(profile))));
+                .Returns(new Func<Model.Profile, Task<Profile?>>(profile => Task.FromResult(SimulatedAddProfile(profile))));
+            
             service.Setup(service => service.UpdateProfile(It.IsAny<Model.Profile>()))
-                .Returns(new Func<Model.Profile, Task<bool>>(profile => Task.FromResult(SimulatedAddProfile(profile))));
+                .Returns(new Func<Model.Profile, Task<bool>>(profile => Task.FromResult(SimulatedUpdateProfile(profile))));
+            
             service.Setup(service => service.getNbProfiles())
                 .Returns(new Func<Task<int>>(() => Task.FromResult(3)));
             _profileController = new SimpleProfileController(loggerApi, service.Object);
+        }
+
+        /// <summary>
+        /// Simule la méthode UpdateProfile de notre service Profile
+        /// </summary>
+        /// <param name="profile">Le Profile a update</param>
+        /// <returns>true si bien updaté, false sinon </returns>
+        private bool SimulatedUpdateProfile(Profile profile)
+        {
+            var profileUp = CreateDatasetProfile().Where(profileWhere => profileWhere.Id == profile.Id).FirstOrDefault();
+            if (profileUp == null)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         /// <summary>
@@ -53,15 +79,15 @@ namespace Api.UnitTests
         /// </summary>
         /// <param name="profile">Le profil à ajouter</param>
         /// <returns>true si bien ajouté, false sinon</returns>
-        private bool SimulatedAddProfile(Profile profile)
+        private Profile? SimulatedAddProfile(Profile profile)
         {
             var myProfile = CreateDatasetProfile().Where(innerProfile => innerProfile.Id == profile.Id).FirstOrDefault();
             if (profile == null)
             {
-                return true;
+                return profile;
             }
 
-            return false;
+            return null;
         }
 
         /// <summary>
@@ -69,7 +95,7 @@ namespace Api.UnitTests
         /// </summary>
         /// <param name="id">L'ID du profil à supprimer</param>
         /// <returns>true si bien supprimé, false sinon</returns>
-        private bool SimulatedRemoveDiceById(Guid id)
+        private bool SimulatedRemoveProfileById(Guid id)
         {
                 var profile= CreateDatasetProfile().Where(profile => profile.Id == id).FirstOrDefault();
                 if (profile == null)
@@ -92,6 +118,161 @@ namespace Api.UnitTests
                 new SimpleProfile(Guid.Parse("76a6268b-cc5f-4971-90d9-079f3720c0b9"),"Grienenberger","Cocome"),
                 new SimpleProfile(Guid.Parse("dcce4943-25ea-4929-a23a-427fd0a62cb7"),"Malvezin","Neitah"),
             };
+        }
+
+
+        /// <summary>
+        /// Jeu de données pour notre test sur la méthode GetProfileByPage.
+        /// </summary>
+        /// <returns></returns>
+        private static IEnumerable<object[]> Test_GetData_GetProfileByPage()
+        {
+            yield return new object[]
+            {
+                1, 3, 200, string.Empty, new
+                    {
+                        Profiles = CreateDatasetProfile().ToDTO(),
+                        PageNumber = 1,
+                        NbElementsByPage= 3,
+                        numberOfElements = 3
+                    }, ""
+            };
+            yield return new object[]
+            {
+                -1, 3, 400, "Please give a page number and a number of elements by page both superior to 0", null, ""
+            };
+            yield return new object[]
+            {
+                1, -3, 400, "Please give a page number and a number of elements by page both superior to 0", null, ""
+            };
+            yield return new object[]
+            {
+                2, 3, 200, string.Empty, new
+                    {
+                        Profiles = new List<Api.DTOs.Profile>(),
+                        PageNumber = 2,
+                        NbElementsByPage= 3,
+                        numberOfElements = 3
+                    }, ""
+            };
+            yield return new object[]
+            {
+                1, 3, 200, string.Empty, new
+                    {
+                        Profiles = CreateDatasetProfile().ToDTO().Where(profile => profile.Surname.Contains("Nei")).ToList(),
+                        PageNumber = 1,
+                        NbElementsByPage= 3,
+                        numberOfElements = 3
+                    }, "Nei"
+            };
+            yield return new object[]
+            {
+                1, 3, 200, string.Empty, new
+                    {
+                        Profiles = CreateDatasetProfile().ToDTO().Where(profile => profile.Surname.Contains("AUYFEUYAFDYUAFE")).ToList(),
+                        PageNumber = 1,
+                        NbElementsByPage= 3,
+                        numberOfElements = 3
+                    }, "AUYFEUYAFDYUAFE"
+            };
+        }
+
+        
+        [TestMethod]
+        [DynamicData(nameof(Test_GetData_GetProfileByPage), DynamicDataSourceType.Method)]
+        public async Task UT_GetProfileByPage(int numPage, int nbByPage, int expectedStatusCode, string expectedMessageError, dynamic answer,string substr) 
+        {
+
+            var result = (await _profileController.GetProfileByPage(numPage, nbByPage, substr)) as ObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedStatusCode, result.StatusCode);
+
+            if (result.StatusCode == 400)
+            {
+                Assert.AreEqual(expectedMessageError, result.Value as string);
+            }
+            else
+            {
+                Assert.AreEqual(result.Value?.ToString(), answer.ToString());
+            }
+        }
+
+
+        private static IEnumerable<object[]> Test_GetData_GetProfileById()
+        {
+            yield return new object[]
+            {
+                Guid.Parse("6af0ae0f-fb3a-4604-810a-9517d8f6a741"), 
+                200,
+                string.Empty,
+                new Model.SimpleProfile(Guid.Parse("6af0ae0f-fb3a-4604-810a-9517d8f6a741"),"Louis","Perret")
+            };
+            yield return new object[]
+            {
+                Guid.Parse("6af0ae0f-ab3a-4604-810a-9517d8f6a741"),
+                404,
+                "There is no profile with this ID",
+                null
+            };
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(Test_GetData_GetProfileById), DynamicDataSourceType.Method)]
+        public async Task UT_GetProfileById(Guid id, int expectedStatusCode, string expectedMessageError, Model.Profile expected)
+        {
+            var result = (await _profileController.GetProfileById(id)) as ObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedStatusCode, result.StatusCode);
+
+            if (result.StatusCode == 404)
+            {
+                Assert.AreEqual(expectedMessageError, result.Value as string);
+            }
+            else
+            {
+                Assert.AreEqual((result.Value as DTOs.Profile)?.ToModel(), expected);
+            }
+        }
+
+        [TestMethod]
+        public async Task UT_RemoveAllProfiles()
+        {
+            var result = (await _profileController.RemoveAllProfiles()) as OkResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+        }
+
+        private static IEnumerable<object[]> Test_GetData_RemoveProfileById()
+        {
+            yield return new object[]
+            {
+                Guid.Parse("6af0ae0f-fb3a-4604-810a-9517d8f6a741"),
+                200,
+                string.Empty
+            };
+            yield return new object[]
+            {
+                Guid.Parse("6af0ae0f-ab3a-4604-810a-9517d8f6a741"),
+                400,
+                "No profile with this ID exists",
+            };
+        }
+        [TestMethod]
+        [DynamicData(nameof(Test_GetData_RemoveProfileById), DynamicDataSourceType.Method)]
+        public async Task UT_RemoveProfileById(Guid id, int expectedStatusCode, string expectedMessageError)
+        {
+            if (expectedStatusCode == 200)
+            {
+                var result = (await _profileController.RemoveProfileById(id)) as OkResult;
+                Assert.IsNotNull(result);
+                Assert.AreEqual(expectedStatusCode, result.StatusCode);
+            }
+            else { 
+                var result = (await _profileController.RemoveProfileById(id)) as ObjectResult;
+                Assert.IsNotNull(result);
+                Assert.AreEqual(expectedStatusCode, result.StatusCode);
+                Assert.AreEqual(expectedMessageError, result.Value as string);
+            }
         }
     }
 }
